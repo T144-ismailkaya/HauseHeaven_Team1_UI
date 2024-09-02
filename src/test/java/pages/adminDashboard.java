7b5b6d744e503a6cf9795701711b08d6039cd0d2
Levent SEFLEK <leventseflek@gmail.com>
2024-09-02 11:32:54 -0400

    Levent Version 02

--- src/test/java/pages/adminDashboard.java
+++ src/test/java/pages/adminDashboard.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,5 +10,26 @@ public class adminDashboard {
     public adminDashboard (){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy (xpath = "test")
+    public WebElement testlelemti01;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti02;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti03;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti04;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti05;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti06;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti07;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti08;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti09;
+    @FindBy (xpath = "test")
+    public WebElement testlelemti10;
 
 }
