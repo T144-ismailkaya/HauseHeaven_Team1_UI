0eb0d0d82203eb1d4c0876dddbe7d4e3741fb21f
İsmail Kaya <ibosh0034@gmail.com>
2024-09-02 17:59:31 +0300

    first commit

---
+++ src/test/java/tests/US_IK008/TC_01.java
@@ -0,0 +1,14 @@
+package tests.US_IK008;
+
+import org.testng.annotations.Test;
+
+public class TC_01 {
+
+    @Test
+    public void test01(){
+
+        System.out.println("Hello world");
+
+    }
+
+}
