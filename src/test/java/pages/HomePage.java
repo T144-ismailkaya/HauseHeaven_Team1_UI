aeb4f0102665f31b4b36418831be4437a73e83f3
rananurulker <rananurulker@gmail.com>
2024-09-03 21:16:21 +0300

    Rana V1

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,19 @@
 package pages;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 public class HomePage {
+    public HomePage (){PageFactory.initElements(Driver.getDriver(),this);}
+
+
+    //---------------Rana Nur---------------\\
+
+    @FindBy(xpath = "//*[@id='navigation']/div[2]/ul[1]/li[2]")
+    public WebElement HomePageListingLink;
+
+    @FindBy (xpath = "//*[@class='ipt-title']")
+    public WebElement listingPropertiesYazisi;
+
+
 }
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -46,9 +47,12 @@ public class Driver {
                  */
                 default:
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--disable-search-engine-choice-screen");
+                    driver = new ChromeDriver(options);
             }
         }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
