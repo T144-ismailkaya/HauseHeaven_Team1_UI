bbbc6362571afc860ab6b7663f02e0fee7a2772e
İsmail Kaya <ibosh0034@gmail.com>
2024-09-04 13:08:45 +0300

    İsmail Kaya 04/09/2024 Elemntlerin düzeni

--- src/test/java/pages/AdminDashboard.java
+++ src/test/java/pages/AdminDashboard.java
@@ -11,27 +11,89 @@ public class AdminDashboard {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    // [Hause Heaven Admin] - [Login fonksiyonu] Bölümünün elementleri
+
+    @FindBy(xpath = "//*[@name='username']")
+    public WebElement usernameTextbox;
+
+    @FindBy(xpath = "//*[@name='password']")
+    public WebElement passwordAdminTextbox;
+
+    @FindBy(className = "signin")
+    public WebElement signInButonu;
+
+    @FindBy(xpath = "(//*[@class='breadcrumb-item active'])[1]")
+    public WebElement settingsSayfaDogrulama;
 
     //---------------İsmail Kaya---------------\\
-    @FindBy (xpath = "test02")
-    public WebElement  testelementbuttonuElementi;
+    @FindBy(xpath = "//*[normalize-space(text())='Real Estate']")
+    public WebElement realEstateButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Properties']")
+    public WebElement propertiesButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Projects']")
+    public WebElement projectsButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Features']")
+    public WebElement featuresButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Facilities']")
+    public WebElement facilitiesButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Investors']")
+    public WebElement investorsButonu;
+
+    @FindBy(xpath = "(//*[normalize-space(text())='Categories'])[2]")
+    public WebElement categoriesButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Types']")
+    public WebElement typesButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Reviews']")
+    public WebElement reviewsButonu;
+
+    @FindBy(xpath = "//*[normalize-space(text())='Settings']")
+    public WebElement settingsButonu;
+
+    @FindBy(xpath = "(//*[@class='breadcrumb-item active'])[2]")
+    public WebElement sayfaDogrulama;
+
+    //-------------İsmail Kaya Son--------------\\
+
     //---------------Levent Seflek---------------\\
+
     @FindBy (xpath = "test01")
     public WebElement  seftestelementbuttonuElementi;
-    //---------------Merve Nur---------------\\
+
+    //---------------Levent Seflek Son------------\\
+
+    //---------------Dilek---------------\\
+
+    //---------------Dilek Son---------------\\
+
+    //---------------Mustafa İsmail--------------\\
+
+    //---------------Mustafa İsmail Son---------------\\
 
     //---------------Rana Nur---------------\\
 
+    //---------------Rana Nur Son---------------\\
+
+    //---------------Bünyamin---------------\\
+
+    //---------------Bünyamin Son---------------\\
+
+    //---------------Fatma (FDM)---------------\\
+
+    //---------------Fatma (FDM) Son---------------\\
+
     //---------------Mehmet Muavviz---------------\\
 
-    //---------------Bünyamin Sait---------------\\
+    //---------------Mehmet Muavviz Son---------------\\
 
-    //---------------Mustafa İsmail---------------\\
+    //---------------Merve Nur---------------\\
 
-    //---------------Dilek ---------------\\
-
-    //---------------Fatma demir---------------\\
-
-    //---------------Sarkan Adiguzelov---------------\\
+    //---------------Merve Nur Son---------------\\
 
 }
