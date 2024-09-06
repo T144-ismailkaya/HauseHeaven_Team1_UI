9fb6ed2c334a15a73ca1b5779c3dbc446457384d
İsmail Kaya <ibosh0034@gmail.com>
2024-09-06 19:49:28 +0300

    T144 06/09/2024 Herkese ait page oluşturulması

---
+++ src/test/java/pages/TeamPages/MustafaPage.java
@@ -0,0 +1,12 @@
+package pages.TeamPages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class MustafaPage {
+
+    public MustafaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+}
