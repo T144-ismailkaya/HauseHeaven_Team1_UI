36a835c149fa93c39a953b0ca6e2ce95e08b2bff
Mervenurembel <mervenurembel@gmail.com>
2024-09-06 22:01:56 +0300

    mervenur 06.09.2024 US022 yazıldı

--- src/test/java/tests/US_011/TC_ME04.java
+++ src/test/java/tests/US_011/TC_ME04.java
@@ -9,35 +9,57 @@ import pages.HomePage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
 import java.awt.*;
 
-public class TC_ME04 {
+public class TC_ME04 extends TestBaseRapor {
 
     @Test
-    public  void contactadresidogrulugutesti(){
+    public  void test04(){
         HomePage homePage=new HomePage();
+        Actions actions=new Actions(Driver.getDriver());
 
-        //Ziyaretçi HauseHeaven url girer.
+        extentTest=extentReports.createTest("adresindogrulugutesti");
+
+
+        extentTest .pass("Ziyaretçi HauseHeaven url girer.");
         Driver.getDriver().get(ConfigReader.getProperty("hauseUrl"));
 
-       // Ziyaretçi HauseHeaven anasayfa header a ulaşır.
+        extentTest .pass(" Ziyaretçi HauseHeaven anasayfa header a ulaşır.");
         Assert.assertTrue(homePage.hauseheavenlogo.isDisplayed());
 
-       // Ziyaretçi Contact linkine tıklar.
+        extentTest .pass("Ziyaretçi Contact linkine tıklar.");
         homePage.contactlinkibutonu.click();
 
-      //  Ziyaretçi Contact sayfasına ulaşır.
+        extentTest .pass(" Ziyaretçi Contact sayfasına ulaşır.");
         ReusableMethods.hover(homePage.contactdırections);
-      // Ziyaretçi sayfada Reach Us kısmındaki adresi görür.
+
+        extentTest .pass(" Ziyaretçi sayfada Reach Us kısmındaki adresi görür.");
 
         Assert.assertTrue(homePage.contactreachuselementi.isDisplayed());
-        // Ziyaretçi adres dogrulamak için Directions kısmındaki harita alanından scrool yaparak Reach Us kısmındaki adresin aynı olduğunu dogrular.
+        extentTest .pass(" Ziyaretçi adres dogrulamak için Directions kısmındaki harita alanından scrool yaparak Reach Us kısmındaki adresin aynı olduğunu dogrular.");
+
+        ReusableMethods.hover(homePage.contactsingontoday);
+
+        ReusableMethods.bekle(2);
 
 
 
-        //Ziyaretçi açılan sekmeyi kapatır.
-        Driver.getDriver().quit();
+        Driver.getDriver().switchTo().frame(homePage.contcatiframe);
+        homePage.contcatiframe.click();
+        homePage.contcatiframe.click();
+        homePage.contcatiframe.click();
+        homePage.contcatiframe.click();
+
+       //actions.doubleClick(homePage.contactharitagoruntule).perform();
+
+
+        ReusableMethods.bekle(3);
+
+
+        extentTest .pass("Ziyaretçi açılan sekmeyi kapatır.");
+       Driver.getDriver().quit();
 
 
     }
---
+++ src/test/java/tests/US_022/TC_ME02.java
@@ -0,0 +1,55 @@
+package tests.US_022;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
+
+import java.util.Random;
+
+public class TC_ME02 extends TestBaseRapor {
+
+    @Test
+    public void test02(){
+
+        HomePage homePage=new HomePage();
+        extentTest=extentReports.createTest(" listingde mulk detaylarin aulastıgı testi");
+        extentTest.pass(" Kayıtlı kullancı HauseHeaven url girer.");
+        Driver.getDriver().get(ConfigReader.getProperty("hauseUrl"));
+
+        extentTest.pass(" Kayıtlı kullanıcı HauseHeaven anasayfa header a ulaşır.");
+        Assert.assertTrue(homePage.hauseheavenlogo.isDisplayed());
+
+       Faker faker=new Faker();
+        homePage.signinButonu.click();
+        String fakeEmail=faker.internet().emailAddress();
+        String fakeSifre=faker.internet().password();
+        homePage.emailTextbox.sendKeys(fakeEmail);
+        homePage.passwordTextbox.sendKeys(fakeSifre);
+        homePage.loginButonu.click();
+
+
+
+        extentTest.pass(" Kayıtlı kullanıcı listing linkine tıklar.");
+        homePage.listingButonu.click();
+
+        extentTest.pass("  Kayıtlı kullanıcı listing sayfasına ulaşır.");
+        Assert.assertTrue(homePage.listingfindbybutonu.isDisplayed());
+        extentTest.pass("Kayıtlı kullanıcı sayfada seçtiği herhangi bir mülke tıklar.");
+        Random random=new Random();
+
+        homePage.listingseciliilan.click();
+        extentTest.pass(" Kayıtlı kullanıcı seçtiği mülkün detaylarına ulaşabilğini doğrular");
+        ReusableMethods.hover(homePage.listingilangalery);
+        Assert.assertTrue(homePage.listingilangalery.isDisplayed());
+
+        extentTest.pass("  Kayıtlı kullanıcı açılan sekmeyi kapatır.");
+
+        Driver.getDriver().quit();
+
+    }
+}
