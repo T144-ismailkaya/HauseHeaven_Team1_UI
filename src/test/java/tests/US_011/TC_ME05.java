ed75c0cc265af91206bb47afb4d4755c36973c1b
Mervenurembel <mervenurembel@gmail.com>
2024-09-08 21:59:42 +0300

    mervenur 08.09.2024 test caseler yazıldı

--- src/test/java/tests/US_011/TC_ME05.java
+++ src/test/java/tests/US_011/TC_ME05.java
@@ -1,2 +1,55 @@
-package tests.US_011;public class TC_ME05 {
+package tests.US_011;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class TC_ME05 {
+    @Test
+
+    public void merve(){
+
+
+
+        HomePage homePage=new HomePage();
+        Actions actions=new Actions(Driver.getDriver());
+
+        Driver.getDriver().get(ConfigReader.getProperty("hauseUrl"));
+        Assert.assertTrue(homePage.hauseheavenlogo.isDisplayed());
+        homePage.contactlinkibutonu.click();
+        Assert.assertTrue(homePage.contactreachuselementi.isDisplayed());
+
+
+        //Levent  Yol
+        WebElement iframe = Driver.getDriver().findElement(By.xpath("//iframe[contains(@src, 'maps')]"));
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", iframe);
+        Driver.getDriver().switchTo().frame(iframe);
+
+        WebElement map = Driver.getDriver().findElement(By.xpath("//body"));
+        ReusableMethods.hover(map);
+        ReusableMethods.wait(3);
+        actions.click(map).perform();
+        actions.click(map).perform();
+        actions.click(map).perform();
+
+
+
+
+        //   Driver.getDriver().switchTo().frame(harita);
+        // ReusableMethods.hover(homePage.contactsingontoday);
+        // ReusableMethods.bekle(2);
+        // Driver.getDriver().switchTo().frame(homePage.contcatiframe);
+        // homePage.contcatiframe.click();
+        // homePage.contcatiframe.click();
+        // homePage.contcatiframe.click();
+        // homePage.contcatiframe.click();
+        //actions.doubleClick(homePage.contactharitagoruntule).perform();
+    }
 }
