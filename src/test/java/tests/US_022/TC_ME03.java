ebfb2d4b25cf78ef531b463927baa9b360ddc328
İsmail Kaya <ibosh0034@gmail.com>
2024-09-11 17:31:43 +0300

    Mervenur 11.09.2024 test caseler yazıldı

--- src/test/java/tests/US_022/TC_ME03.java
+++ src/test/java/tests/US_022/TC_ME03.java
@@ -17,38 +17,31 @@ public class TC_ME03 extends TestBaseRapor {
         extentTest=extentReports.createTest("secilen mulkun facebook  platforma yonlendirildigi testi ");
         extentTest.pass(" Kayıtlı kullancı HauseHeaven url girer.");
         Driver.getDriver().get(ConfigReader.getProperty("hauseUrl"));
-
         extentTest.pass(" Kayıtlı kullanıcı HauseHeaven anasayfa header a ulaşır.");
+
         Assert.assertTrue(homePage.hauseheavenlogo.isDisplayed());
+        homePage.sefUsercookies.click();
         homePage.signinButonu.click();
         homePage.singinemailbutonu.sendKeys("elif@gmail.com");
         homePage.singinpasswordbutonu.sendKeys("123456789.elif");
         homePage.singinloginbutonu.click();
 
-
         extentTest.pass("Kayıtlı kullanıcı listing linkine tıklar. ");
         homePage.listingButonu.click();
-
         extentTest.pass("  Kayıtlı kullanıcı listing sayfasına ulaşır.");
         Assert.assertTrue(homePage.listingfindbybutonu.isDisplayed());
 
-       extentTest.pass("Kayıtlı kullanıcı sayfada seçtiği herhangi bir mülke tıklar. ") ;
+        extentTest.pass("Kayıtlı kullanıcı sayfada seçtiği herhangi bir mülke tıklar. ") ;
         homePage.listingseciliilan.click();
         extentTest.pass(" Kayıtlı kullanıcı seçtiği mülkün detaylarına ulaşabilğini doğrular");
         ReusableMethods.hover(homePage.listingilangalery);
+
         Assert.assertTrue(homePage.listingilangalery.isDisplayed());
-
-       //homePage.listingsayfasiilanfacebookElementi.click();
-       // homePage.listingsayfasiilanfacebookElementi.click();
-
         extentTest.pass(" Kayıtlı kullanıcı mük sayfasında share butonunun üzerine gelerek facebook ikonunu üzerine tıklayarak yeni sekmede facebook  platformuna gonderdiğini dogrular");
-
         ReusableMethods.hover(homePage.listingshare);
         homePage.listingfacebookbutonu.click();
 
         extentTest.pass(" Kayıtlı kullanıcı açılan sekmeyi kapatır.");
-
         Driver.getDriver().quit();
-
     }
 }
